29e6fcf178e1d73d9b8987b47be4420cc7785eed
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2023-05-27 09:26:31 -0500

    actualizacion de la fecha por defecto en la creacion de la tabla sopadeletras

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./db.sqlite");
 
 db.run(
-  "CREATE TABLE IF NOT EXISTS sopaletras (id INTEGER PRIMARY KEY AUTOINCREMENT, Juego TEXT, Colaborador TEXT, Tiempo TEXT, FechaRegistro DATETIME DEFAULT CURRENT_TIMESTAMP)"
+  "CREATE TABLE IF NOT EXISTS sopaletras (id INTEGER PRIMARY KEY AUTOINCREMENT, Juego TEXT, Colaborador TEXT, Tiempo TEXT, FechaRegistro DATETIME DEFAULT (datetime('now','localtime')))"
 );
 
 module.exports = db;
