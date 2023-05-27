f80cdc396944056a9705f61fa052938f32187d37
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2023-05-27 09:05:24 -0500

    codigo innecesario borrado

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const bodyParser = require("body-parser");
 const db = require("./db");
 
 const app = express();
@@ -11,11 +10,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/new", (req, res) => {
-  console.log(req.body);
   const { Juego, Colaborador, Tiempo } = req.body;
   db.run(
     `INSERT INTO sopaletras (Juego, Colaborador, Tiempo) VALUES ("${Juego}", "${Colaborador}", "${Tiempo}")`
