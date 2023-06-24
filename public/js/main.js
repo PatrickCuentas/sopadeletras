70bf0f42b7b8852d67724ed36ecbcd3c64b3dabb
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2023-06-23 20:31:26 -0500

    v1

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
     letras_probadas,
     intervalCrono,
     numeroColaborador,
+    numeroIntentos = 0,
     cantidad_pistas = 2,
     aciertos = 0,
     fallos = 0,
@@ -52,12 +53,23 @@ $(document).ready(function () {
     return `${minutes}:${parseSeconds} min`;
   };
 
+  function reiniciarVariablesGlobales() {
+    palabra_secreta = "";
+    letras_probadas = [];
+    aciertos = 0;
+    fallos = 0;
+    time = 0;
+  }
+
   function inicializar() {
     $("button").prop("disabled", false);
+    reiniciarVariablesGlobales();
     palabra_secreta = palabras[Math.floor(Math.random() * palabras.length)];
 
-    $("#imagen_ahorcado").removeClass("hidden");
-    $("#imagen_ahorcado").attr("src", `img/stick_game/fallo0.png`).addClass("sm:w-1/2");
+    $("#imagen_ahorcado")
+      .attr("src", `img/stick_game/fallo0.png`)
+      .addClass("sm:w-[70%]");
+
     insertarPalabra();
     generarTeclado();
     asignarEventosTeclado();
@@ -126,7 +138,8 @@ $(document).ready(function () {
     palabraContainer.html("");
     $.each(palabraRandon, function (index, letra) {
       let spanLetra = $("<div>").addClass(
-        "p-[6px] h-[30px] m-[3px] sm:p-3 sm:m-2 bg-[#fff] text-black rounded-md text-sm text-center sm:text-2xl sm:h-auto"
+        // "p-[6px] h-[30px] m-[3px] sm:p-3 sm:m-2 bg-[#fff] text-black rounded-md text-sm text-center sm:text-2xl sm:h-auto"
+        "w-[25px] h-[25px] md:w-[35px] md:h-[35px] text-sm md:text-md bg-[#fff] flex justify-center items-center text-black rounded-md"
       );
       if (letra === " ") {
         spanLetra = $("<br>");
@@ -140,14 +153,13 @@ $(document).ready(function () {
 
   function countLetter(word, letter) {
     let count = 0;
-    for(let i in word){
-      if(word[i] === letter){
-          count++;
+    for (let i in word) {
+      if (word[i] === letter) {
+        count++;
       }
     }
     return count;
   }
-
 
   function insertarPistaPalabraSecreta() {
     // 1. Obtener las "letras" de la "palabra_secreta" en base a la cantidad
@@ -160,7 +172,7 @@ $(document).ready(function () {
     // 2. Insertar las letras en el contenedor de la palabra oculta
     for (let i = 0; i < palabra_secreta.length; i++) {
       if (posiciones.has(i)) {
-        if(countLetter(palabra_secreta, palabra_secreta.charAt(i)) === 1){
+        if (countLetter(palabra_secreta, palabra_secreta.charAt(i)) === 1) {
           $(".boton-probar").each(function () {
             let letra = $(this).text().trim().toUpperCase();
             if (letra === palabra_secreta.charAt(i)) {
@@ -172,8 +184,6 @@ $(document).ready(function () {
         escribirSpan(i, palabra_secreta.charAt(i));
       }
     }
-
-    //TODO: 3. Validar si la letra descubierta es la unica para deshabilitar el boton de la letra
   }
 
   function escribirSpan(indice, letra) {
@@ -196,10 +206,10 @@ $(document).ready(function () {
         letras_probadas += letra;
       }
     }
-  
-    let sinEspacios = palabra_secreta.replace(new RegExp(" ", "g"), "")
 
-    if (aciertos == sinEspacios.length ) {
+    let sinEspacios = palabra_secreta.replace(new RegExp(" ", "g"), "");
+
+    if (aciertos == sinEspacios.length) {
       gane();
     }
   }
@@ -209,10 +219,10 @@ $(document).ready(function () {
 
     letras_probadas += letra;
 
-    $("#imagen_ahorcado").attr("src", `/img/stick_game/fallo${fallos}.png`);
-
     if (fallos == 7) {
       perdida();
+    } else {
+      $("#imagen_ahorcado").attr("src", `/img/stick_game/fallo${fallos}.png`);
     }
   }
 
@@ -220,7 +230,7 @@ $(document).ready(function () {
     const Juego = "Ahorcado";
     const Colaborador = numeroColaborador;
     const Tiempo = formatTime(time);
-    const body = { Juego, Colaborador, time };
+    const body = { Juego, Colaborador, Tiempo };
 
     // $("#imagen_ahorcado").attr(
     //   "src",
@@ -229,20 +239,39 @@ $(document).ready(function () {
     makeRequest(body);
     stopTimer();
     $("#modal3").toggleClass("hidden");
-    $('#title').text('¡Felicidades!')
-    $('#content').html('<p>Has resuelto el juego en <span id="tiempo-juego"></span>.</p>')
+    $("#title").text("¡Felicidades!");
+    $("#content").html(
+      '<p>Has resuelto el juego en <span id="tiempo-juego"></span>.</p>'
+    );
     $("#tiempo-juego").text(Tiempo);
   }
 
   function perdida() {
     stopTimer();
+    numeroIntentos++;
     $("#modal3").toggleClass("hidden");
-    $('#title').text('¡Fallaste!')
-    $('#content').html('<p>Tienes una oportunidad más para encontrar la frase.</p><button id="btn-reintentar" class="btn btn-primary">Reintentar</button>')
-    $("#btn-reintentar").on("click", function () {
-      inicializarJuego();
-    });
-    $("#tiempo-juego").text(Tiempo);
+    $("#title").text("¡Fallaste!");
+
+    if (numeroIntentos === 1) {
+      $("#content").html(
+        `<p>Tienes una oportunidad más para encontrar la frase.</p>
+     <div class="flex justify-center mt-2">
+      <button id="btn-reintentar" class="bg-[#d41073] text-white text-sm md:text-md lg:text-lg p-2 md:p-3 rounded-[12px] min-w-[70px]">
+        OK
+      </button>
+     </div>
+    `
+      );
+      $("#btn-reintentar").on("click", function () {
+        inicializar();
+        startTimer();
+        $("#modal3").toggleClass("hidden");
+      });
+    } else {
+      $("#content").html(`
+        <p>Suerte para la proxima.</p>
+      `);
+    }
   }
 
   function desabilitarLetra(ref) {
@@ -266,17 +295,23 @@ $(document).ready(function () {
       ["z", "x", "c", "v", "b", "n", "m"],
     ];
 
+    $("#keyboard").empty();
+
     // Loop through each row of the keyboard
     for (let row of keyboard) {
       // Create a new row element
-      let $row = $("<section>").addClass("flex gap-2 justify-start sm:justify-center flex-wrap my-1 sm:my-2 w-full sm:gap-4 ");
+      let $row = $("<section>").addClass(
+        "flex gap-2 justify-start sm:justify-center flex-wrap my-1 sm:my-2 w-full sm:gap-4 "
+      );
 
       // Loop through each key in the row
       for (let key of row) {
         // Create a new button element
         let $button = $("<button>")
           .addClass(
-            "boton-probar p-4 bg-[#fff] text-center font-bold rounded-md text-gray-600 cursor-pointer disabled:bg-gray-500 disabled:text-white disabled:cursor-not-allowed text-[15px]"
+            `boton-probar p-2 sm:px-[1rem] sm:py-[.5rem] bg-[#fff] text-center font-bold rounded-md text-gray-600
+            cursor-pointer disabled:bg-neutral-300 disabled:text-gray-700 
+            disabled:cursor-not-allowed text-[14px] sm:text-[16px] font-bold flex-1 sm:flex-none`
           )
           .text(key);
 
@@ -299,7 +334,6 @@ $(document).ready(function () {
       // });
       $(this).click(function () {
         probarLetra($(this), letra);
-        console.log("Aciertos: ", aciertos)
       });
     });
   }
@@ -324,4 +358,3 @@ $(document).ready(function () {
  ?? TODO
   *- imagen de ganar o perder
 */
-
